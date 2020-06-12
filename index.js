5f2127a62bb821abd8fb0fdd6d95279143bf7d72
mehdichamouma <mehdi.chamouma@gmail.com>
2020-06-11 20:54:10 -0500

    Change port

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var app = express();
+const PORT = process.env.PORT || 5000
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", function (req, res) {
     res.send("hello world");
 });
 
-app.listen(3000, function () {
-    console.log("Example app listening on port 3000!");
+app.listen(PORT, function () {
+    console.log("Example app listening on port PORT!");
 });
